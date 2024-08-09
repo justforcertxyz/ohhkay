a0169f008635553f88cb15749ceac301aeb41d69
ok <ok@ohhkay.de>
2024-08-09 23:55:05 +0200

    add landing:index view

--- ohhkay/landing/tests.py
+++ ohhkay/landing/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from unittest import skip
+from django.urls import reverse
 
-# Create your tests here.
+
+class IndexPageTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.index_url = reverse('landing:index')
+
+    def test_index_page_returns_correct_response(self):
+        response = self.client.get(self.index_url)
+        self.assertTemplateUsed(response, 'landing/index.html')
+        self.assertTemplateUsed(response, 'landing/base.html')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_index_page_returns_corrent_content(self):
+        response = self.client.get(self.index_url)
+        self.assertContains(response, "<title>Home")
