5a8ea51fa113406aacfee32f54df8d99a0cd8148
ok <ok@ohhkay.de>
2024-08-10 13:59:05 +0200

    changed blog model to FileField instead of TextField

--- ohhkay/blog/models.py
+++ ohhkay/blog/models.py
@@ -6,9 +6,15 @@ class Blog(models.Model):
     title = models.CharField("Post Title", max_length=50, unique=True)
     pub_date = models.DateTimeField("Date Published", default=timezone.now)
     slug = models.SlugField("Slug", max_length=50, unique=True)
-    text = models.TextField("Blog Text", max_length=6000, default="")
+    html_file = models.FileField("HTML File",
+        upload_to=f"blog/templates/blog/entries/",
+        null=True, blank=True
+    )
 
     tags = TaggableManager()
 
     def __str__(self):
         return self.title
+
+    def template_path(self):
+        return self.html_file.name.split("templates/")[1]
--- ohhkay/blog/tests.py
+++ ohhkay/blog/tests.py
@@ -2,9 +2,21 @@ from django.test import TestCase, Client
 from unittest import skip
 from .models import Blog
 from django.urls import reverse
-
+import os
+from django.conf import settings
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 class BlogModelTest(TestCase):
+    def setUp(self):
+        folder = f"{settings.BASE_DIR}/blog/templates/blog/entries"
+        if len(os.listdir(folder)) > 0:
+            os.system(f'rm {folder}/test*')
+
+    def tearDown(self):
+        folder = f"{settings.BASE_DIR}/blog/templates/blog/entries"
+        if len(os.listdir(folder)) > 0:
+            os.system(f'rm {folder}/test*')
+
     def test_Blog_model_exists(self):
         blog_count = Blog.objects.count()
 
@@ -16,6 +28,21 @@ class BlogModelTest(TestCase):
         blog = Blog.objects.create(title=title, slug=slug)
 
         self.assertEqual(str(blog), blog.title)
+    
+    def test_template_path(self):
+        file_name = "test.html"
+        html_file = SimpleUploadedFile(name=file_name, 
+            content=b"<h1>This is some HTML</h1><p>Here is some pragraph</p>",
+            content_type="text/html"
+        )
+
+        blog = Blog.objects.create(title="Some Title",
+            slug="some_slug",
+            html_file=html_file,
+        )
+
+        self.assertEqual(blog.template_path(), "blog/entries/test.html")
+    
 
 class IndexPageTest(TestCase):
     def setUp(self):
@@ -35,9 +62,29 @@ class IndexPageTest(TestCase):
 
 class DetailPageTest(TestCase):
     def setUp(self):
+        folder = f"{settings.BASE_DIR}/blog/templates/blog/entries"
+        if len(os.listdir(folder)) > 0:
+            os.system(f'rm {folder}/test*')
+
         self.client = Client()
-        self.blog = Blog.objects.create(title="Some Blog", slug="some_slug")
+        self.file_name = "test.html"
+        self.html_file = SimpleUploadedFile(name=self.file_name, 
+            content=b"<h1>This is some HTML</h1><p>Here is some pragraph</p>",
+            content_type="text/html"
+        )
+
+        self.blog = Blog.objects.create(title="Some Title",
+            slug="some_slug",
+            html_file=self.html_file,
+        )
+
         self.detail_url = reverse('blog:detail', kwargs={'slug': self.blog.slug})
+
+
+    def tearDown(self):
+        folder = f"{settings.BASE_DIR}/blog/templates/blog/entries"
+        if len(os.listdir(folder)) > 0:
+            os.system(f'rm {folder}/test*')
 
     def test_index_page_returns_correct_response(self):
         response = self.client.get(self.detail_url)
@@ -48,3 +95,4 @@ class DetailPageTest(TestCase):
     def test_index_page_returns_corrent_content(self):
         response = self.client.get(self.detail_url)
         self.assertContains(response, f"<title>{self.blog.title}")
+        self.assertContains(response, '<h1>This is some HTML</h1><p>Here is some pragraph</p>')
