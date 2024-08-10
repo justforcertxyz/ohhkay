074b793768553298ba7f49ed95be4418c84220c0
ok <ok@ohhkay.de>
2024-08-10 12:48:34 +0200

    add blog:detail page

--- ohhkay/blog/tests.py
+++ ohhkay/blog/tests.py
@@ -31,3 +31,20 @@ class IndexPageTest(TestCase):
     def test_index_page_returns_corrent_content(self):
         response = self.client.get(self.index_url)
         self.assertContains(response, "<title>Blog")
+
+
+class DetailPageTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.blog = Blog.objects.create(title="Some Blog", slug="some_slug")
+        self.detail_url = reverse('blog:detail', kwargs={'slug': self.blog.slug})
+
+    def test_index_page_returns_correct_response(self):
+        response = self.client.get(self.detail_url)
+        self.assertTemplateUsed(response, 'blog/detail.html')
+        self.assertTemplateUsed(response, 'landing/base.html')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_index_page_returns_corrent_content(self):
+        response = self.client.get(self.detail_url)
+        self.assertContains(response, f"<title>{self.blog.title}")
